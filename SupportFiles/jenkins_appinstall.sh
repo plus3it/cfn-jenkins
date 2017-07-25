1d4131a327455c3803c26944aa6bea391076e65a
Thomas H Jones II <github@xanthia.com>
2017-07-25 18:25:33 -0400

    Fix JENKHOMEURL pathing

--- SupportFiles/jenkins_appinstall.sh
+++ SupportFiles/jenkins_appinstall.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# shellcheck disable=
+# shellcheck disable=SC2005,SC2059
 #
 # Script to install and configure Jenkins and related components
 #
 #################################################################
+# shellcheck disable=SC2086
 PROGNAME="$(basename ${0})"
 SELMODE="$(awk -F= '/^SELINUX=/{print $2}' /etc/selinux/config)"
 JENKDATADIR="${JENKINS_HOME_PATH:-UNDEF}"
 JENKBKUPBKT="${JENKINS_BACKUP_BUCKET:-UNDEF}"
 JENKBKUPFLD="${JENKINS_BACKUP_FOLDER:-UNDEF}"
-JENKHOMEURL="s3://${JENKBKUPBKT}/${JENKBKUPFLD}/backups/sync/JENKINS_HOME/"
-CFNENDPT="${JENKINS_CFN_ENDPOINT:-UNDEF}"
-JENKINITTOK="/var/lib/jenkins/secrets/initialAdminPassword"
+JENKHOMEURL="s3://${JENKBKUPBKT}/${JENKBKUPFLD}"
+JENKINITTOK="${JENKDATADIR}/secrets/initialAdminPassword"
 
 ##
 ## Set up an error logging and exit-state
@@ -37,9 +37,9 @@ yum install -y jenkins || err_exit 'Jenkins install failed'
      
 # Restore JENKINS_HOME content (if available)
 echo "Attempting to restore JENKINS_HOME from S3... "
-su -s /bin/bash jenkins -c "/usr/bin/aws s3 sync --quiet ${JENKHOMEURL} /var/lib/jenkins"
+su -s /bin/bash jenkins -c "/usr/bin/aws s3 sync --quiet ${JENKHOMEURL}/sync/JENKINS_HOME/ ${JENKDATADIR}"
 
-if [[ -f /var/lib/jenkins/config.xml ]]
+if [[ -f ${JENKDATADIR}/config.xml ]]
 then
    echo "Restored JENKINS_HOME from S3."
    FRESHINSTALL=1
@@ -57,16 +57,17 @@ systemctl start jenkins
  crontab -l 2> /dev/null ;
  echo "# Daily backup of JENKINS_HOME to S3" ;
  printf "0 23 * * * su -s /bin/bash jenkins -c " ;
- printf "\"/usr/bin/aws s3 sync --delete --quiet /var/lib/jenkins ${JENKHOMEURL}" ;
- echo "/daily/\$(date '+%A')\"" ;
+ printf "\"/usr/bin/aws s3 sync --delete --quiet ${JENKDATADIR} ${JENKHOMEURL}" ;
+ echo "/daily/\$(date '+%A')/\"" ;
  echo "# Sync JENKINS_HOME to S3 for re-deployments" ;
  printf "*/20 * * * * su -s /bin/bash jenkins -c " ;
- printf "\"/usr/bin/aws s3 sync --delete --quiet /var/lib/jenkins ${JENKHOMEURL}" ;
+ printf "\"/usr/bin/aws s3 sync --delete --quiet ${JENKDATADIR} ${JENKHOMEURL}" ;
  echo "/sync/JENKINS_HOME/\"";
 ) | crontab -
 
 # Re-enable SELinux
 printf "Reverting SELinux enforcing-mode... "
+# shellcheck disable=SC2015
 setenforce "${SELMODE}" && echo || err_exit 'Could not change SEL-mode'
 
 # Display initial admin token on fresh install first-boot
