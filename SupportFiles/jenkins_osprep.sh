62b5831a5651860955b1ff5d3b1e5eb8aeb59a30
Thomas H Jones II <github@xanthia.com>
2017-08-30 10:34:44 -0400

    Rework firewalld rules: 1) Use service-names rather than port-exceptions 2) Create a "jenkins" service (to be used in "1")

--- SupportFiles/jenkins_osprep.sh
+++ SupportFiles/jenkins_osprep.sh
@@ -6,11 +6,15 @@
 # shellcheck disable=SC2086
 PROGNAME="$(basename ${0})"
 EPELREPO="${JENKINS_EPEL_REPO:-UNDEF}"
-FWPORTS=(
-         80
-         443
+FWSVCS=(
+      http
+      https
+      jenkins
+   )
+JNKPORTS=(
          8080
          32000
+         45158
         )
 BONUSRPMS=(
            git
@@ -43,12 +47,34 @@ function err_exit {
 echo "Adding exceptions to firewalld... "
 setenforce 0 || err_exit "Failed to temp-disable SELinux"
 firewall-offline-cmd --enabled
-for PORT in "${FWPORTS[@]}"
+
+## Create Jenkins firewalld rule-set
+printf "Adding new firewalld service-def... "
+firewall-cmd --permanent --new-service=jenkins || \
+  err_exit 'Failed creating firewalld service-name'
+printf "Adding short-description to service-def... "
+firewall-cmd --permanent --service=jenkins --set-short="Jenkins Service Ports" \
+  || err_exit 'Failed adding short-description'
+printf "Adding long-description to service-def... "
+firewall-cmd --permanent --service=jenkins \
+  --set-description="Jenkins service firewalld port exceptions" || \
+    err_exit 'Failed adding long-description'
+
+for PORT in "${JNKPORTS[@]}"
 do
    printf "Adding firewalld exception for %s/tcp... " "${PORT}"
-   firewall-cmd --zone=public --add-port="${PORT}"/tcp --permanent || \
+   firewall-cmd --service=jenkins --add-port="${PORT}"/tcp --permanent || \
      err_exit "Failed to add firewalld exception for ${PORT}/tcp."
 done
+
+## Activate firewalld service-definitions
+for SVC in "${FWSVCS[@]}"
+do
+   printf "Activating %s firewalld service..." "${SVC}"
+   firewall-cmd --permanent --add-service="${SVC}" || \
+     err_exit 'Failed to activate ${SVC} firewalld service'
+done
+
 printf "Reloading firewalld configuration... "
 firewall-cmd --reload || err_exit "Failed reloading firewalld configuration."
 setenforce 1 || err_exit "Failed to re-enable SELinux"
