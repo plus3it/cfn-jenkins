65e809b22fcd84b7e06f612b5d9e79da5bfabd18
Jason Wagner <jason-plus3@users.noreply.github.com>
2018-06-12 11:45:27 -0400

    Moved python package install from the userscript to chain-load.sh where it will run as root

--- SupportFiles/chain-load.sh
+++ SupportFiles/chain-load.sh
@@ -35,6 +35,23 @@ quiet_git() {
    fi
 }
 
+
+# Install Python 2.x, pip, and virtualenv
+yum install -y python python2-pip
+pip2 install -U virtualenv pip
+
+# Install Python3.4, pip, and virtualenv
+yum install -y python34 
+curl https://bootstrap.pypa.io/get-pip.py | python3.4  # no rpm in epel
+pip3.4 install -U virtualenv pip
+
+
+# Install Python 3.6, pip, and virtualenv using the semi-official "Inline with Upstream Stable" repo
+# yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+# yum install -y python36u python36u-pip
+# pip3.6 install -U virtualenv pip
+
+
 # Create git staging-area as needed
 if [[ -d ${SCRIPTHOME}/git ]]
 then
