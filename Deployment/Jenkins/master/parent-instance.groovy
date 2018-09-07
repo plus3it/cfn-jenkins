adfb8e4a350f0c5f5f9b661f23f893ceca3f05af
john.d.murphy <john.murphy@plus3it.com>
2018-09-07 19:07:54 -0400

    changed # to save from 3 to 5

--- Deployment/Jenkins/master/parent-instance.groovy
+++ Deployment/Jenkins/master/parent-instance.groovy
@@ -5,7 +5,7 @@ pipeline {
     options {
         buildDiscarder(
             logRotator(
-                numToKeepStr: '3',
+                numToKeepStr: '5',
                 daysToKeepStr: '30',
                 artifactDaysToKeepStr: '30',
                 artifactNumToKeepStr: '3'
