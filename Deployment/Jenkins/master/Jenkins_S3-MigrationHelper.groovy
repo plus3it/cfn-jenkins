6fef8cf34033ee63e11c1b96c65759f1e8c4fb38
Thomas H Jones II <github@xanthia.com>
2018-09-14 13:43:28 -0400

    Work on reporting logic for source bucket

--- Deployment/Jenkins/master/Jenkins_S3-MigrationHelper.groovy
+++ Deployment/Jenkins/master/Jenkins_S3-MigrationHelper.groovy
@@ -24,7 +24,6 @@ pipeline {
     parameters {
         string(name: 'AwsRegion', defaultValue: 'us-east-1', description: 'Amazon region to deploy resources into')
         string(name: 'AwsCred', description: 'Jenkins-stored AWS credential with which to execute cloud-layer commands')
-        string(name: 'GitCred', description: 'Jenkins-stored Git credential with which to execute git commands')
         string(name: 'SourceBucket', description: 'Bucket to copy contents from')
         string(name: 'DestinationBucket', description: 'Bucket to copy contents to')
         string(name: 'RootFolder', description: 'Which bucket-folder to synchronize (if any)')
@@ -35,19 +34,26 @@ pipeline {
             steps {
                 withCredentials(
                     [
-                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
-                        sshUserPrivateKey(credentialsId: "${GitCred}", keyFileVariable: 'SSH_KEY_FILE', passphraseVariable: 'SSH_KEY_PASS', usernameVariable: 'SSH_KEY_USER')
+                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']
                     ]
                 ) {
                     sh '''#!/bin/bash
                         if [[ -z ${RootFolder} ]]
                         then
+                           echo "Censusing entirety of s3://${SourceBucket}"
+                        else
                            LIMITSCOPE="--prefix ${RootFolder}"
+                           echo "Censusing s3://${SourceBucket}/${RootFolder}"
                         fi
 
-                        aws s3api list-objects --bucket ${SourceBucket} \${LIMITSCOPE} --output json \
-                          --query "[sum(Contents[].Size), length(Contents[])]" | \
-                          awk 'NR!=2 {print $0;next} NR==2 {print $0/1024/1024/1024" GB"}'
+                        BUCKETCOUNTS=($( aws s3api list-objects --bucket ${SourceBucket} \${LIMITSCOPE} --output json --query "[sum(Contents[].Size), length(Contents[])]" | awk 'NR!=2 {print \$0;next} NR==2 {print \$0 / 1024 / 1024 / 1024}' ))
+
+                        # For the console readers...
+                        printf "\tBucket Size: %sGiB\n" "${BUCKETCOUNTS[1]}"
+                        printf "\tBucket objects: %s\n" "${BUCKETCOUNTS[2]}"
+
+                        # Save for later use
+                        echo "SourceBucket ${BUCKETCOUNTS[@1]}" > bucket_census.txt
 
                     '''
                 }
@@ -57,28 +63,24 @@ pipeline {
             steps {
                 withCredentials(
                     [
-                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
-                        sshUserPrivateKey(credentialsId: "${GitCred}", keyFileVariable: 'SSH_KEY_FILE', passphraseVariable: 'SSH_KEY_PASS', usernameVariable: 'SSH_KEY_USER')
+                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']
                     ]
                 ) {
                     sh '''#!/bin/bash
                         echo > /dev/null
-
                     '''
                 }
             }
         }
-        stage ('Copy Bucket') {
+        stage ('Diff Buckets') {
             steps {
                 withCredentials(
                     [
-                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
-                        sshUserPrivateKey(credentialsId: "${GitCred}", keyFileVariable: 'SSH_KEY_FILE', passphraseVariable: 'SSH_KEY_PASS', usernameVariable: 'SSH_KEY_USER')
+                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']
                     ]
                 ) {
                     sh '''#!/bin/bash
                         echo > /dev/null
-
                     '''
                 }
             }
